7358b0903f20a7d9572f59bf445edd00dae4d86c
Richard Clark <richard@redspider.co.nz>
2011-01-22 01:34:41 +1300

    added test

--- toys/simplessh.js
+++ toys/simplessh.js
@@ -16,7 +16,7 @@ loggly.bark.external_command({
 
             var plist = [];
             $.each(result, function (i, r) {
-                plist.push('<tr><td>' + r.ip + '</td><td>' + r.user + '</td></tr>');
+                plist.push('<tr><td style="padding-right: 5px;">' + r.ip + '</td><td>' + r.user + '</td></tr>');
             });
 
             return ['<table><tr><th>IP</th><th>User</th></tr>', plist.join(''), '</table>'].join('');
